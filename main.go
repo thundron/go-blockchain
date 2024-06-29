2c16249d37ac9cf6b400167397422478b94fe874
Lorenzo Alberto Maria Ambrosi <la@thundron.dev>
2024-06-29 17:55:34 +0200

    Add index as ID
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <la@thundron.dev>

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ func main() {
 	blockchain.AddBlock("first transaction") // first block that represents a transaction
 	blockchain.AddBlock("second transaction") // second block that represents a transaction
 	for i, block := range blockchain.Blocks {
-		fmt.Printf("Hash of the %d block: %x", i, block.CurrentBlockHash)
-		fmt.Printf("Hash of the previous %d block: %x", i-1, block.PreviousBlockHash)
+		fmt.Prinf("Block ID: %d\n", i)
+		fmt.Prinf("Timestamp: %d\n", block.Timestamp+int64(i)) // differentiate the block timestamps with the ID
+		fmt.Printf("Hash of the %d block: %x\n", i, block.CurrentBlockHash)
+		fmt.Printf("Hash of the previous %d block: %x\n", i-1, block.PreviousBlockHash)
 		fmt.Printf("All transactions: %s\n", block.AllData)
 	}
 }
