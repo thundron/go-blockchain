c6f0b11b34599a46f78b423a86ad66a2b45b680b
Lorenzo Alberto Maria Ambrosi <la@thundron.dev>
2024-06-29 16:29:19 +0200

    Initialize with gitignore
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <la@thundron.dev>

---
+++ block.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CreateBlock(Header, Body string) {
+	fmt.Println(Header, "\n", Body)
+}
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Blockchain experiment n.1")
+	CreateBlock("Header", "Body")
+}
